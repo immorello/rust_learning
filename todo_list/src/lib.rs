8a3384a20fc1954157703e4f517708252e7aa8bd
Francesco Imovilli <imovilli_f@immergas.com>
2026-05-04 16:49:05 +0200

    developed task addition

--- todo_list/src/lib.rs
+++ todo_list/src/lib.rs
@@ -1,18 +1,63 @@
+use rand::{Rng, RngExt};
 pub enum Choice{
     AddTask,
     ShowTask,
-    CompleTask,
+    CompleteTask,
     DeleteTask,
     Quit
+}
+
+struct Task{
+    id: i32,
+    title: String,
+    completed: bool,
+}
+
+pub struct TodoList{
+    tasks: Vec<Task>,
+}
+
+impl TodoList{
+    pub fn new() -> TodoList{
+        TodoList { tasks: Vec::new() }
+    }
+
+    pub fn add_task(&mut self, title:String){
+        let mut rng = rand::rng();
+        let id = rng.random_range(1..=200);
+        let new_task = Task{
+            id,
+            title,
+            completed:false,
+        }
+        self.tasks.push(new_task);
+        
+    }
 }
 
 pub fn parse_option(num: i32)->Result<Choice, String>{
     match num {
         1 => Ok(Choice::AddTask),
         2 => Ok(Choice::ShowTask),
-        3 => Ok(Choice::CompleTask),
+        3 => Ok(Choice::CompleteTask),
         4 => Ok(Choice::DeleteTask),
         5 => Ok(Choice::Quit),
         _ => Err("Not a valid option!".to_string())
+    }
+}
+
+pub fn start_option(list: &mut TodoList, option:Choice, details:Option<String>){
+    match option {
+       Choice::AddTask => {
+        match details {
+            Some(title)=>list.add_task(title),
+            None => println!("Can't be empty!"),
+        }
+       },
+       Choice::ShowTask => show_task(),
+       Choice::CompleteTask => complete_task(),
+       Choice::DeleteTask => delete_task(),
+       Choice::Quit => quit(),
+
     }
 }
--- todo_list/src/main.rs
+++ todo_list/src/main.rs
@@ -1,10 +1,16 @@
+use core::task;
 use std::io;
+use todo_list::Choice;
+use todo_list::TodoList;
+use todo_list::parse_option;
+use todo_list::start_option;
 
 fn main() {
+    let mut todo_list = TodoList::new();
     println!("Welcome to your Rust todo list!");
     println!("How can i help you today?");
     println!("1.Add Task\n2.Show Task\n3.Complete Task\n4.Delete Task\n5.Quit");
-    let option_number: i32 = loop {
+    let option: Choice = loop {
         let mut option_string: String = String::new();
         match io::stdin().read_line(&mut option_string) {
             Ok(_) => {}
@@ -13,11 +19,38 @@ fn main() {
                 continue;
             }
         }
-        match option_string.trim().parse() {
-            Ok(num) => break num,
-            Err(_) => println!("Not a valid number, try again"),
+        let option_number = match option_string.trim().parse() {
+            Ok(num) => {
+                println!("You selected option {}", num);
+                num
+            },
+            Err(_) => {
+                println!("Not a valid number, try again");
+                continue;
+            }
+        };
+
+        match parse_option(option_number) {
+            Ok(option) => break option,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
         }
     };
 
-    println!("You selected option {}", option_number);
+    match option {
+        Choice::AddTask => {
+            let task_title: String = String::new();
+            match io::stdin().read_line(&mut task_title){
+                Ok(_)=>start_option(&mut todo_list, Choice::AddTask, Some(String::from(task_title))),
+                Err(_)=>{println!("Errore nell'inserimento del task")}
+            }
+        },
+        Choice::ShowTask => start_option(&mut todo_list, option, details),
+        Choice::CompleteTask => start_option(&mut todo_list, option, details),
+        Choice::DeleteTask => start_option(&mut todo_list, option, details),
+        Choice::Quit => start_option(&mut todo_list, option, details),
+    }
+    
 }
