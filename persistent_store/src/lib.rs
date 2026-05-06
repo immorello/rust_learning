c8c2677a845645eaf01d46d65fe18cd261dde348
Francesco Imovilli <imovilli_f@immergas.com>
2026-05-06 10:44:58 +0200

    first step, copied the old exercise in order to update it

---
+++ persistent_store/src/lib.rs
@@ -0,0 +1,164 @@
+use std::collections::HashMap;
+use std::io;
+
+pub enum Command {
+    Set,
+    Get,
+    Delete,
+    List,
+    Quit,
+}
+
+pub enum Value {
+    Integer(i32),
+    Float(f64),
+    Text(String),
+    Boolean(bool),
+}
+
+pub struct Store {
+    data: HashMap<String, Value>,
+}
+
+impl Store {
+    pub fn new() -> Store {
+        Store {
+            data: HashMap::new(),
+        }
+    }
+
+    fn set_value(&mut self, new_key: String, new_value: Value) {
+        self.data.insert(new_key.clone(), new_value);
+        println!("Inserted value with key {}", new_key);
+
+        match self.data.get(&new_key) {
+            Some(new_value) => match new_value {
+                Value::Integer(num) => println!("Value for item with key {}: {}\n", new_key, num),
+                Value::Float(num) => println!("Value for item with key {}: {}\n", new_key, num),
+                Value::Text(txt) => println!("Value for item with key {}: {}\n", new_key, txt),
+                Value::Boolean(bool) => println!("Value for item with key {}: {}\n", new_key, bool),
+            },
+            None => println!("Recently inserted value not found!"),
+        }
+    }
+
+    fn get_value(&self, key: &str) -> Option<&Value> {
+        self.data.get(key)
+    }
+
+    fn delete_value(&mut self, key: &str) -> Option<Value> {
+        self.data.remove(key)
+    }
+
+    fn list_values(&self) {
+        if self.data.is_empty() {
+            println!("Store is empty");
+            return;
+        }
+        println!("Here's the complete list of items in the store:\n");
+        for (key, value) in &self.data {
+            match value {
+                Value::Integer(num) => println!("Value for item with key {}: {}\n", key, num),
+                Value::Float(num) => println!("Value for item with key {}: {}\n", key, num),
+                Value::Text(txt) => println!("Value for item with key {}: {}\n", key, txt),
+                Value::Boolean(bool) => println!("Value for item with key {}: {}\n", key, bool),
+            }
+        }
+    }
+}
+
+pub fn parse_value(input: &str) -> Value {
+    if let Ok(num) = input.parse::<i32>() {
+        Value::Integer(num)
+    } else if let Ok(num) = input.parse::<f64>() {
+        Value::Float(num)
+    } else if let Ok(bool) = input.parse::<bool>() {
+        Value::Boolean(bool)
+    } else {
+        Value::Text(input.to_string())
+    }
+}
+
+pub fn parse_option(option_number: i32) -> Result<Command, String> {
+    match option_number {
+        1 => Ok(Command::Set),
+        2 => Ok(Command::Get),
+        3 => Ok(Command::Delete),
+        4 => Ok(Command::List),
+        5 => Ok(Command::Quit),
+        _ => Err("Not a valid option!".to_string()),
+    }
+}
+
+pub fn execute_command(
+    store: &mut Store,
+    command: Command,
+    key: Option<String>,
+    value: Option<Value>,
+) {
+    match command {
+        Command::Set => match key {
+            Some(key) => match value {
+                Some(value) => {
+                    store.set_value(key, value);
+                }
+                None => println!("Value cannot be empty!"),
+            },
+            None => println!("Key cannot be empty!"),
+        },
+        Command::Get => match key {
+            Some(key) => match store.get_value(&key) {
+                Some(value) => match value {
+                    Value::Integer(num) => println!("Value for item with key {}: {}\n", key, num),
+                    Value::Float(num) => println!("Value for item with key {}: {}\n", key, num),
+                    Value::Text(txt) => println!("Value for item with key {}: {}\n", key, txt),
+                    Value::Boolean(bool) => println!("Value for item with key {}: {}\n", key, bool),
+                },
+                None => println!("Couldn't find any value with key {}", key),
+            },
+            None => println!("Key cannot be empty!"),
+        },
+        Command::Delete => match key {
+            Some(key) => match store.delete_value(&key) {
+                Some(_) => println!("Deleted value with key {}", key),
+                None => println!("Could not delete value with key {}", key),
+            },
+            None => println!("Key cannot be empty!"),
+        },
+        Command::List => {
+            store.list_values();
+        }
+        Command::Quit => {
+            println!("Bye bye!");
+            std::process::exit(0)
+        }
+    }
+}
+
+pub fn insert_key() -> String {
+    let mut new_empty_key: String = String::new();
+    loop {
+        println!("Insert the value of the key you want to save\n");
+        match io::stdin().read_line(&mut new_empty_key) {
+            Ok(_) => break new_empty_key,
+            Err(_) => {
+                println!("Error while reading key!");
+                continue;
+            }
+        }
+    }
+}
+
+pub fn insert_value() -> Value {
+    let mut new_value: String = String::new();
+    loop {
+        println!("Insert the value to associate to the key\n");
+        match io::stdin().read_line(&mut new_value) {
+            Ok(_) => break parse_value(&new_value),
+            Err(_) => {
+                println!("Error while reading value");
+                continue;
+            }
+        }
+    }
+}
--- persistent_store/src/main.rs
+++ persistent_store/src/main.rs
@@ -1,3 +1,81 @@
+use persistent_store::Command;
+use persistent_store::Store;
+use persistent_store::Value;
+use persistent_store::execute_command;
+use persistent_store::insert_key;
+use persistent_store::insert_value;
+use persistent_store::parse_option;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the key-value store written in Rust\n");
+    let mut new_store = Store::new();
+    loop {
+        println!("Choose the command:\n");
+        println!("1:set\n2:get\n3:delete\n4:list\n5:quit\n");
+
+        let option: Command = loop {
+            let mut option_string: String = String::new();
+            match io::stdin().read_line(&mut option_string) {
+                Ok(_) => {}
+                Err(_) => {
+                    println!("Error while reading the character\n");
+                    continue;
+                }
+            };
+            let option_number: i32 = match option_string.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Not a valid number!");
+                    continue;
+                }
+            };
+            match parse_option(option_number) {
+                Ok(option) => break option,
+                Err(error) => {
+                    println!("{}", error);
+                    continue;
+                }
+            }
+        };
+
+        match option {
+            Command::Set => {
+                let new_key: String = insert_key();
+                println!("Inserted key: {}", new_key);
+                let new_value: Value = insert_value();
+
+                execute_command(
+                    &mut new_store,
+                    Command::Set,
+                    Some(String::from(new_key)),
+                    Some(Value::from(new_value)),
+                );
+            }
+            Command::Get => {
+                let new_key: String = insert_key();
+                execute_command(
+                    &mut new_store,
+                    Command::Get,
+                    Some(String::from(new_key)),
+                    None,
+                );
+            }
+            Command::Delete => {
+                let new_key: String = insert_key();
+                execute_command(
+                    &mut new_store,
+                    Command::Delete,
+                    Some(String::from(new_key)),
+                    None,
+                );
+            }
+            Command::List => {
+                execute_command(&mut new_store, Command::List, None, None);
+            }
+            Command::Quit => {
+                execute_command(&mut new_store, Command::Quit, None, None);
+            }
+        }
+    }
 }
