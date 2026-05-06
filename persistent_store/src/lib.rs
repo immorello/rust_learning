67c5728e0266993460ac71f54aba977a206d07d9
Francesco Imovilli <imovilli_f@immergas.com>
2026-05-06 12:32:05 +0200

    added serveral persisting functions

--- persistent_store/src/lib.rs
+++ persistent_store/src/lib.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
-use std::io;
-
+use std::{collections::HashMap, fs::File};
+use std::io::{self, Read, Write};
+use serde::{Serialize, Deserialize};
+use std::fs;
+const STORAGE_PATH: &str = "./Storage/storage.json";
 pub enum Command {
     Set,
     Get,
@@ -8,22 +10,65 @@ pub enum Command {
     List,
     Quit,
 }
-
+#[derive(Serialize, Deserialize)]
 pub enum Value {
     Integer(i32),
     Float(f64),
     Text(String),
     Boolean(bool),
 }
-
+#[derive(Serialize, Deserialize)]
 pub struct Store {
     data: HashMap<String, Value>,
 }
 
 impl Store {
+    
     pub fn new() -> Store {
         Store {
             data: HashMap::new(),
+        }
+    }
+
+    fn serialize(&self)-> String{
+        let json_res = serde_json::to_string(&self);
+        match json_res {
+            Ok(json)=>json,
+            Err(_)=>"Could not serialize json".to_string(),
+        }
+    }
+
+    fn deserialize(&self, store_string:&str)->Option<Store>{
+        let my_store = serde_json::from_str(store_string);
+        match my_store {
+            Ok(store)=>store,
+            Err(_)=>None,
+        }
+    }
+
+    fn persist_to_file(&self){
+        let store_string = self.serialize();
+        let file = std::fs::File::create(STORAGE_PATH);
+        match file {
+            Ok(mut new_file)=>{
+                let write_result = new_file.write_all(store_string.as_bytes());
+                match write_result {
+                    Ok(_)=>println!("data persisted to file"),
+                    Err(_)=>println!("Error while printing to file"),
+                }
+            },
+            Err(_)=>println!("Could not create the file"),
+        }
+    }
+
+    fn read_from_file(&self)->Option<Store>{
+        let file_to_read = fs::read_to_string(STORAGE_PATH);
+        match file_to_read {
+            Ok(string_file)=>{
+                let store = self.deserialize(&string_file);
+                store
+            },
+            Err(_)=>None,
         }
     }
 
